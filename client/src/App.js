c16ea6053892b5ef21e5a461865bd40589c5aa58
Melissa Manousos <mmanousos@gmail.com>
2020-06-11 12:24:00 -0700

    Working: form toggle

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,60 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-// import Form from "./components/Form";
-// import Verification from "./components/Verification";
-// import Confirmation from "./components/Confirmation";
-import MainContainer from "./components/MainContainer";
+import Form from "./components/Form";
 
 class App extends Component {
   state = {
     response: "",
     post: "",
     responseToPost: "",
+    account: "",
+    verified: false,
   };
 
-  componentDidMount() {
-    this.callApi()
-      .then((res) => this.setState({ response: res.express }))
-      .catch((err) => console.log(err));
-  }
+  // componentDidMount() {
+  //   this.callApi()
+  //     .then((res) => this.setState({ response: res.express }))
+  //     .catch((err) => console.log(err));
+  // }
+  // componentDidMount() {
+  //   this.callApi()
+  //     .then((res) => this.setState({ account: res.express }))
+  //     .catch((err) => console.log(err));
+  // }
 
-  callApi = async () => {
-    const response = await fetch("/api/hello");
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
+  // callApi = async () => {
+  //   const response = await fetch("/api/hello");
+  //   const body = await response.json();
+  //   if (response.status !== 200) throw Error(body.message);
 
-    return body;
-  };
+  //   return body;
+  // };
+  // callApi = async () => {
+  //   const response = await fetch("/verify");
+  //   const body = await response.json();
+  //   if (response.status !== 200) throw Error(body.message);
 
-  handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetch("/api/world", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
+  //   return body;
+  // };
 
-    this.setState({ responseToPost: body });
+  // handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const response = await fetch("/api/world", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ post: this.state.post }),
+  //   });
+  //   const body = await response.text();
+
+  //   this.setState({ responseToPost: body });
+  // };
+
+  redirectVerify = () => {
+    console.log("I will redirect you now");
+    // redirect to /verify
   };
 
   render() {
@@ -49,52 +65,7 @@ class App extends Component {
         </header>
         <Router>
           <Switch>
-            <Route
-              exact
-              path="/"
-              render={(props) => (
-                <MainContainer
-                  {...props}
-                  component={"home"}
-                  title={"Enter Your Details"}
-                />
-              )}
-            />
-            <Route
-              path="/edit"
-              render={(props) => (
-                <MainContainer
-                  {...props}
-                  component={"edit"}
-                  title={"Edit Your Details"}
-                />
-              )}
-            />
-            <Route
-              path="/verify"
-              render={(props) => (
-                <MainContainer
-                  {...props}
-                  component={"verify"}
-                  title={"Verify Details"}
-                  msg={
-                    "Click 'edit' to correct or 'confirm' if they are accurate."
-                  }
-                />
-              )}
-            />
-            <Route
-              path="/confirm"
-              render={(props) => (
-                <MainContainer
-                  {...props}
-                  component={"confirm"}
-                  title={"Success!"}
-                  msg={"Your account is now active."}
-                />
-              )}
-            />
-
+            <Route exact path="/" component={Form} />
             <Route
               render={() => (
                 <div>
@@ -105,8 +76,6 @@ class App extends Component {
             />
           </Switch>
         </Router>
-
-        <p>{this.state.response}</p>
       </div>
     );
   }
@@ -120,10 +89,66 @@ export default App;
     onChange={(e) => this.setState({ post: e.target.value })}
   /> 
   
-  <p>{this.state.responseToPost}</p>
+  <p>{this.state.account}</p>
   */
 
-// main path title = 'Sign Up'
-// edit path title = 'Edit'
-// verify path title = 'Verify'
-// confirmation path title = 'Success'
+//  <Router>
+//  <Switch>
+//    <Route
+//      exact
+//      path="/"
+//      render={(props) => (
+//        <MainContainer
+//          {...props}
+//          component={"home"}
+//          title={"Enter Your Details"}
+//          redirect={this.redirectVerify}
+//        />
+//      )}
+//    />
+//    <Route
+//      path="/edit"
+//      render={(props) => (
+//        <MainContainer
+//          {...props}
+//          component={"edit"}
+//          title={"Edit Your Details"}
+//        />
+//      )}
+//    />
+//    <Route
+//      path="/verify"
+//      render={(props) => (
+//        <MainContainer
+//          {...props}
+//          component={"verify"}
+//          title={"Verify Details"}
+//          msg={
+//            "Click 'edit' to correct or 'confirm' if they are accurate."
+//          }
+//          // add redirectEdit and redirectSubmit functions / props
+//        />
+//      )}
+//    />
+//    <Route
+//      path="/confirm"
+//      render={(props) => (
+//        <MainContainer
+//          {...props}
+//          component={"confirm"}
+//          title={"Success!"}
+//          msg={"Your account is now active."}
+//        />
+//      )}
+//    />
+
+//    <Route
+//      render={() => (
+//        <div>
+//          <h2>Oh no!</h2>
+//          <h3>I'm sorry. You've found a page with no content.</h3>
+//        </div>
+//      )}
+//    />
+//  </Switch>
+// </Router>
